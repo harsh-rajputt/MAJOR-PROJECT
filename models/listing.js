3497d624a43287ae0cb2426a8dd9b602baa6969f
Harsh raj <harshraj9162192367@gmail.com>
2024-09-26 18:34:24 +0530

    phase 1

---
+++ models/listing.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+    title: {
+       type: String,
+       required: true,
+    },
+    description: String,
+    image: {
+       type: String,
+       default: "https://images.unsplash.com/photo-1471357674240-e1a485acb3e1?q=80&w=875&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+       set: (v) => 
+        v === ""
+        ? "https://images.unsplash.com/photo-1471357674240-e1a485acb3e1?q=80&w=875&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+        : v,
+    },
+    price: {
+        type: Number,
+        required: true,
+        min: [0, 'Price must be a positive number']
+    },
+    location: {
+        type: String,
+        required: true,
+    },
+    country: {
+        type: String,
+        required: true,
+    },
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
