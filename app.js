baea60e2bc1949b5b6906c3bbdb960a56264657a
Harsh raj <harshraj9162192367@gmail.com>
2024-09-28 12:20:38 +0530

    Structure complete

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
+const methodOverride = require("method-override");
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -17,6 +18,7 @@ async function main() {
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 app.get("/", (req, res) => {
     res.send("Hi, I am root");
@@ -59,6 +61,22 @@ app.get("/listings/:id/edit", async (req, res) => {
     let {id} = req.params;
     const listing = await Listing.findById(id);
     res.render("edit.ejs",{listing});
+});
+
+//Update Route
+
+app.put("/listings/:id", async (req, res) => {
+    let {id} = req.params;
+    await Listing.findByIdAndUpdate(id, {...req.body.listing});
+    res.redirect(`/listings/${id}`);
+});
+
+//Delete Route
+
+app.delete("/listings/:id", async (req, res) => {
+    let {id} = req.params;
+  let deleteListing = await Listing.findByIdAndDelete(id);
+    res.redirect("/listings");
 });
 
 
