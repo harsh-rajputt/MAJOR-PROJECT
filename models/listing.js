cb8abd78bcee48ca36cd0c02e7430c0bd198ac49
Harsh raj <harshraj9162192367@gmail.com>
2024-09-30 19:11:57 +0530

    style complete

--- models/listing.js
+++ models/listing.js
@@ -5,8 +5,12 @@ const listingSchema = new Schema({
     title: {
        type: String,
        required: true,
+       trim: true // Removes extra spaces before and after the title
     },
-    description: String,
+    description: {
+        type: String,
+        trim: true
+    },
     image: {
         filename: {
           type: String,
@@ -16,7 +20,7 @@ const listingSchema = new Schema({
           type: String,
           default: "https://images.unsplash.com/photo-1471357674240-e1a485acb3e1?q=80&w=875&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         }
-      },
+    },
     price: {
         type: Number,
         required: true,
@@ -25,12 +29,14 @@ const listingSchema = new Schema({
     location: {
         type: String,
         required: true,
+        trim: true
     },
     country: {
         type: String,
         required: true,
-    },
-});
+        trim: true
+    }
+}, { timestamps: true }); // Automatically adds createdAt and updatedAt
 
 const Listing = mongoose.model("Listing", listingSchema);
 module.exports = Listing;
